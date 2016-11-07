b76f2f7c7c0a2e7b485111edb76e22000e9829ef
dz <git.daniele@gmail.com>
2016-11-08 00:18:16 +0100

    AI to compute configuration which do not overlap

--- compute.py
+++ compute.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 
+# This script computes a possible configuration for eight tables using
+# genetic algorithms so that they do not overlap.
+# Fitness function is based only on overlapping.
+
 # XXX rotate tables
-# XXX check that radius is 100cm
-# XXX AI
+# XXX fitness function: include escape paths
+# XXX fitness function: increase average distance of tables
+# XXX fitness function: direction to door / other collegues
+
 
 import sys, random
 from io import load_room, load_table, load_escape
 from helpers import put_table, put_tables, cp_room, get_target_pos, dist
-from helpers import find_valid_random_table_layout
+from helpers import find_valid_random_table_layout, find_random_table_layout
+from helpers import put_tables_with_collision
 
 n_tables = 0
 table_pos = [(10, 20), (30, 25)]
 
-#n_tables = 6
-#table_pos = []
+n_tables = 8
+table_pos = []
 room = []
 table = []
 table_start_offset = []   # start position of employee relative to left upper corner of table
@@ -42,13 +49,13 @@ def big_enough(r, x, y):
 			if c != '-':
 				if not allowed(r, x + xpos, y + ypos):
 					return False
-	# XXX search error
 	return True
 
 def do_compute_path(r, cx, cy, tx, ty, n, stack, visited):
-	#if n > 200:
-	#	stack.append((tx, ty))
-	#	return
+	if n > 700:
+		print >> sys.stderr, "stack limit"
+		#stack.append((tx, ty))
+		return
 	stack.append((cx, cy))
 	candidates = []
 	for y in [-1, 0, 1]:
@@ -63,7 +70,6 @@ def do_compute_path(r, cx, cy, tx, ty, n, stack, visited):
 				stack.append((tx, ty))
 				return
 			visited.add((x, y))
-			#r[y][x] = 'O'
 			do_compute_path(r, x, y, tx, ty, n + 1, stack, visited)
 			if stack[-1] == (tx, ty):
 				return
@@ -109,18 +115,152 @@ room                      = load_room(open("raum.txt"))     # load room layout f
 target_pos                = get_target_pos(room)
 table, table_start_offset = load_table()                    # load table layout from file
 
-# initial population
-if n_tables > 0:
-	table_pos             = find_valid_random_table_layout(n_tables, room, table)
+r = None
 
-# put one population into the room
-r = put_tables(room, table, table_pos)                   # place tables in room
-if not r:
-	# this should not happen
-	print >> sys.stderr, "invalid table configuration"
-	sys.exit(1)
+class individuum:
+	def __init__(self, table_pos):
+		self.fitness_value = 0.0
+		self.room_config = None
+		self.table_pos = table_pos
 
-r = compute_paths(r, table, table_pos)
+if False:
+	# initial population
+	if n_tables > 0:
+		table_pos = find_valid_random_table_layout(n_tables, room, table)
+		# put one population into the room
+		r = put_tables(room, table, table_pos)              # place tables in room
+	if not r:
+		# this should not happen
+		print >> sys.stderr, "invalid table configuration"
+		sys.exit(1)
+	r = compute_paths(r, table, table_pos)
+
+
+def tobin(i):
+	b = []
+	# 7 bit for x
+	# 6 bit for y
+	for x, y in i.table_pos:
+		for i in [64, 32, 16, 8, 4, 2, 1]:
+			if x & i:
+				b.append(1)
+			else:
+				b.append(0)
+		for i in [32, 16, 8, 4, 2, 1]:
+			if y & i:
+				b.append(1)
+			else:
+				b.append(0)
+	return b
+
+def split_bin(b):
+	for i in xrange(0, len(b), 7 + 6):
+		yield b[i:i+13]
+
+def fb(v):
+	r = 0
+	f = 1
+	for i in reversed(v):
+		r += f * i
+		f *= 2
+	return r
+
+def frombin(b):
+	r = []
+	for i in split_bin(b):
+		x = fb(i[:7])
+		y = fb(i[7:])
+		r.append((x, y))
+	return individuum(r)
+
+
+def mutation(i):
+	b = tobin(i)
+	n = len(b)
+	for i in xrange(n):
+		if random.randint(0, n - 1) == 0:  # with prob = 1/n
+			b[i] = 1 - b[i]                # flip bit
+	return frombin(b)
+
+def crossover(i, j):
+	ibin = tobin(i)
+	jbin = tobin(j)
+	p = random.randint(1, len(ibin) - 1)
+	a1 = ibin[:p]
+	a2 = jbin[p:]
+	b2 = ibin[p:]
+	b1 = jbin[:p]
+	a = frombin(a1 + a2)
+	b = frombin(b1 + b2)
+	return (a, b)
+
+
+if True:
+	population = []
+	n = 100
+	n_elitism = 1         # only 1 is allowed
+	n_selection = 29
+	n_crossover = 60 / 2
+	n_mutation = 10
+	# create initial population
+
+	for i in xrange(n):
+		# can also return overlapping tables
+		table_pos = find_random_table_layout(n_tables, room, table)
+		population.append(individuum(table_pos))
+
+	while True:
+		assert(len(population) == n)
+
+		# compute fitness function for population
+		fsum = 0
+		fsumr = 0.0
+		best = 0
+		for (idx, i) in enumerate(population):
+			# cnt = number of collisions
+			#print >> sys.stderr, i.table_pos
+			r, cnt = put_tables_with_collision(room, table, i.table_pos)
+			i.room_config = r
+			i.fitness_value = cnt
+			fsum += cnt
+			fsumr += 1.0 / (cnt + 1)
+			if cnt < population[best].fitness_value:
+				best = idx
+		print >> sys.stderr, "average fitness:", float(fsum) / n, "best:", population[best].fitness_value
+
+		if population[best].fitness_value == 0:
+			tp = population[best].table_pos
+			r, cnt = put_tables_with_collision(room, table, tp)
+			break
+
+		new_pop = []
+		# elitism: keep the best indiviuum
+		new_pop.append(population[best])
+
+		# selection
+		for i in xrange(n_selection):
+			rnd = random.random()
+			s = 0.0
+			for j in population:
+				s += 1.0 / (j.fitness_value + 1) / fsumr
+				if s >= rnd:
+					new_pop.append(j)
+					break
+
+		# crossover
+		for i in xrange(n_crossover):
+			rnda = random.randint(0, n - 1)
+			rndb = random.randint(0, n - 1)
+			a, b = crossover(population[rnda], population[rndb])
+			new_pop.append(a)
+			new_pop.append(b)
+
+		# mutation
+		for i in xrange(n_mutation):
+			j = random.randint(0, n - 1)
+			new_pop.append(mutation(population[j]))
+
+		population = new_pop
 
 
 # output room and table setting with escape paths
--- helpers.py
+++ helpers.py
@@ -49,3 +49,49 @@ def find_valid_random_table_layout(n_tables, room, table):
 			pos.append((x, y))
 		if len(pos) == n_tables:
 			return pos
+
+# ------------------------------------
+
+def put_tables_with_collision(room, table, table_pos):
+	r = cp_room(room)
+	cnt = 0
+	for x, y in table_pos:
+		x = put_table_in_room_with_collision(r, table, x, y, True)
+		#if x == -1:
+			#print >> sys.stderr, "error"
+			#sys.exit(1)
+		#	x = 10000 # TODO high penalty
+		cnt += x
+	return r, cnt
+
+def put_table_in_room_with_collision(room, table, x, y, border = False):
+	collisions = 0
+	for (ypos, l) in enumerate(table):
+		for (xpos, c) in enumerate(l):
+			if c != ' ':
+				if not border:
+					if ypos + y >= len(room) or xpos + x >= len(room[ypos + y]):
+						return -1
+					if room[ypos + y][xpos + x] != 'X':
+						collisions += 1
+				if border:
+					if ypos + y >= len(room) or xpos + x >= len(room[ypos + y]):
+						collisions += 1
+					else:
+						if room[ypos + y][xpos + x] != 'X':
+							collisions += 1
+						room[ypos + y][xpos + x] = c
+	return collisions
+
+def find_random_table_layout(n_tables, room, table):
+	while True:
+		pos = []
+		r = cp_room(room)
+		for i in xrange(n_tables):
+			x = random.randint(0, len(room[0]) - 1)
+			y = random.randint(0, len(room) - 1)
+			if put_table_in_room_with_collision(r, table, x, y) == -1:
+				break
+			pos.append((x, y))
+		if len(pos) == n_tables:
+			return pos
